8dbab1e46a4b15b55f687a53ca810d8f22aa7115
Alexey Astafyev <astafyev@restream.rt.ru>
2018-02-06 14:12:38 +0400

    split REST handlers from main function

---
+++ handlers.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Status string   `json:"status"`
+	Data   JsonData `json:"data"`
+}
+
+type JsonResponse interface {
+	toJson() string
+}
+
+func send_sms(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	defer request.Body.Close()
+
+	sms_data := jsonFromRequestBody(request)
+	query_params := map[string]string{
+		"operation": "send",
+		"login":     gateway_params["login"],
+		"password":  gateway_params["password"],
+		"shortcode": gateway_params["shortcode"],
+		"msisdn":    fmt.Sprintf("7%s", sms_data["phone"]),
+		"text":      sms_data["text"],
+	}
+
+	log.Print("Request url: ", gateway_params["url"])
+	log.Print("Request params: ", query_params)
+
+	gw_resp := send_request(gateway_params, query_params)
+
+	log.Print("Gateway Response Status: ", gw_resp.Status())
+	log.Print("Gateway Response Body: ", gw_resp.String())
+
+	resp := &Response{Data: make(map[string]string)}
+
+	if gw_resp.StatusCode() == http.StatusOK && sms_id_regexp.MatchString(gw_resp.String()) {
+		resp.Status = "success"
+		resp.Data["sms_id"] = gw_resp.String()
+	} else {
+		resp.Status = "error"
+		resp.Data["error"] = gw_resp.String()
+	}
+
+	fmt.Fprintf(writer, resp.toJson())
+}
+
+func get_sms_status(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	defer request.Body.Close()
+
+	query_params := map[string]string{
+		"operation": "status",
+		"login":     gateway_params["login"],
+		"password":  gateway_params["password"],
+		"id":        params.ByName("id"),
+	}
+
+	log.Print("Request url: ", gateway_params["url"])
+	log.Print("Request params: ", query_params)
+
+	gw_resp := send_request(gateway_params, query_params)
+
+	log.Print("Gateway Response Status: ", gw_resp.Status())
+	log.Print("Gateway Response Body: ", gw_resp.String())
+
+	resp := &Response{Data: make(map[string]string)}
+
+	if gw_resp.StatusCode() == http.StatusOK {
+		resp.Status = "success"
+		resp.Data["delivery_status"] = gw_resp.String()
+	} else {
+		resp.Status = "error"
+		resp.Data["error"] = gw_resp.String()
+	}
+
+	fmt.Fprintf(writer, resp.toJson())
+}
+
+func (resp Response) toJson() string {
+	result, err := json.Marshal(resp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(result)
+}
--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 	"regexp"
 )
-
-type Response struct {
-	Status string   `json:"status"`
-	Data   JsonData `json:"data"`
-}
-
-type JsonResponse interface {
-	toJson() string
-}
 
 const sms_id_format = "^[0-9]+$"
 
@@ -33,6 +22,7 @@ func main() {
 	gateway_params["login"] = getenv("SMS_GATEWAY_LOGIN", "")
 	gateway_params["password"] = getenv("SMS_GATEWAY_PASSWORD", "")
 	gateway_params["shortcode"] = getenv("SMS_GATEWAY_SHORTCODE", "")
+
 	sms_id_regexp, _ = regexp.Compile(sms_id_format)
 
 	router := httprouter.New()
@@ -44,79 +34,4 @@ func main() {
 	if err := http.ListenAndServe(addr, router); err != nil {
 		log.Fatal("Could not run SMS sender: ", err)
 	}
-}
-
-func send_sms(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	defer request.Body.Close()
-
-	sms_data := jsonFromRequestBody(request)
-	query_params := map[string]string{
-		"operation": "send",
-		"login":     gateway_params["login"],
-		"password":  gateway_params["password"],
-		"shortcode": gateway_params["shortcode"],
-		"msisdn":    fmt.Sprintf("7%s", sms_data["phone"]),
-		"text":      sms_data["text"],
-	}
-
-	log.Print("Request url: ", gateway_params["url"])
-	log.Print("Request params: ", query_params)
-
-	gw_resp := send_request(gateway_params, query_params)
-
-	log.Print("Gateway Response Status: ", gw_resp.Status())
-	log.Print("Gateway Response Body: ", gw_resp.String())
-
-	resp := &Response{Data: make(map[string]string)}
-
-	if gw_resp.StatusCode() == http.StatusOK && sms_id_regexp.MatchString(gw_resp.String()) {
-		resp.Status = "success"
-		resp.Data["sms_id"] = gw_resp.String()
-	} else {
-		resp.Status = "error"
-		resp.Data["error"] = gw_resp.String()
-	}
-
-	fmt.Fprintf(writer, resp.toJson())
-}
-
-func get_sms_status(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	defer request.Body.Close()
-
-	query_params := map[string]string{
-		"operation": "status",
-		"login":     gateway_params["login"],
-		"password":  gateway_params["password"],
-		"id":        params.ByName("id"),
-	}
-
-	log.Print("Request url: ", gateway_params["url"])
-	log.Print("Request params: ", query_params)
-
-	gw_resp := send_request(gateway_params, query_params)
-
-	log.Print("Gateway Response Status: ", gw_resp.Status())
-	log.Print("Gateway Response Body: ", gw_resp.String())
-
-	resp := &Response{Data: make(map[string]string)}
-
-	if gw_resp.StatusCode() == http.StatusOK {
-		resp.Status = "success"
-		resp.Data["delivery_status"] = gw_resp.String()
-	} else {
-		resp.Status = "error"
-		resp.Data["error"] = gw_resp.String()
-	}
-
-	fmt.Fprintf(writer, resp.toJson())
-}
-
-func (resp Response) toJson() string {
-	result, err := json.Marshal(resp)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return string(result)
 }
